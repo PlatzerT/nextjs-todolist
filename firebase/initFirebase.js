89bc972fb799effbcb3fc48d3ba9a91741a8ba27
Thomas Platzer <thomas.platzer@htl-wels.at>
2021-04-18 10:08:54 +0200

    🔥 initialized firebase

---
+++ firebase/initFirebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+export default function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+    console.log('initialized!');
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import Layout from '../components/Layout'
-import '../styles/tailwind.css'
+import Layout from '../components/Layout';
+import '../styles/tailwind.css';
+import firebase from '../firebase/initFirebase';
+
+firebase();
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  ) 
+  );
 }
 
-export default MyApp
+export default MyApp;
